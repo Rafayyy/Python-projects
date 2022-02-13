42ac1a0ae2ed3c3f51b71a9bffe85b0dbda00827
Rafayy <93051673+Rafayyy@users.noreply.github.com>
2022-02-13 13:06:24 +0500

    Update guess game.py

--- guess game.py
+++ guess game.py
@@ -1,12 +1,13 @@
-from random import randint
-secret_num=randint(1,10)
-count=0
-guess=0
-while guess!=secret_num and count <=4:
+##This is a simple guess game by using 100 random numbers
+from random import randint 
+secret_num=randint(1,100) ##The random answer is stored in "secret_num" variable
+count=0 ##used to keep track of number of turns user takes
+guess=0 ##initilaizedg guess to compare with secret_num variable
+while guess!=secret_num and count <=4: ##while condition for user to enter guess 5 times while the guess is not equal to the answer
     guess=eval(input("Enter your guess:"))
     count=count+1
     if guess<secret_num:
-        print("Higher!",5-count,'guesses left')
+        print("Higher!",5-count,'guesses left') 
     elif guess>secret_num:
         print("Lower!",5-count,'guesses left')
     else:
